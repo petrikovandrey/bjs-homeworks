e834a0ead96ceb4692b1854c0ca4e2b3cc37cebb
Andrey Petrikov <aapetrikov@gmail.com>
2019-11-09 14:37:05 +0200

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,17 @@
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+let myName = "Andrey";
+let message = (`Привет! Меня зовут ${myName}`);
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
