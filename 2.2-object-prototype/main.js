a27ad341d6b1e56d0937bbd4eacda7ae67aea6b8
Andrey Petrikov <aapetrikov@gmail.com>
2020-01-03 21:18:31 +0200

    1-2 done

--- 2.2-object-prototype/main.js
+++ 2.2-object-prototype/main.js
@@ -3,11 +3,21 @@ function initCheckBirthday() {
 
     const result = checkBirthday(birthday) ? "Да" : "Нет";
 
-    document.getElementById('disclaimer').innerHTML = result;   
+    document.getElementById('disclaimer').innerHTML = result;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+    let date = new Date(birthday);
+    let now = new Date();
+    let diff = Math.floor(now.getTime() - date.getTime());
+    let year = 1000 * 60 * 60 * 24 * 365;
+    let age = diff / year;
+   
+    console.log(date);
+    console.log(diff);
+    console.log(year);
+    console.log(age);
+    return age >= 18;
 }
 
 function initPrintAnimalSound() {
@@ -17,11 +27,24 @@ function initPrintAnimalSound() {
 
     const result = getAnimalSound(animal);
 
-    document.getElementById('sound').innerHTML = result;   
+    document.getElementById('sound').innerHTML = result;
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
+    if (checkUndefined(animal)) {
+        return null;
+    };
+    let sound = animal.sound;
+    if (!checkUndefined(sound)) {
+        return sound;
+    };
+}
+function checkUndefined(param) {
+    if (typeof param == "undefined") {
+        return true;
+    } else {
+        return false;
+    };
 }
 
 function initCalculateStatement() {
@@ -35,5 +58,27 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
+
+    let sum = 0,
+        total = 0;
+    for (let key in marks) {
+        sum = getAverage(marks[key]);
+        marks[key] = sum;
+        // console.log(key + ": " + marks[key]);
+        total += sum;
+    };
+    marks.average = total / Object.keys(marks).length;
+    let roundedAverage = Math.round(marks.average);
+    console.log(roundedAverage);
+    return roundedAverage;
+}
+
+function getAverage(mark) {
+    let sum = 0;
+
+    for (let i = 0; i < mark.length; i++) {
+        sum += mark[i];
+    };
+    //console.log(sum / mark.length);
+    return sum / mark.length;
 }
