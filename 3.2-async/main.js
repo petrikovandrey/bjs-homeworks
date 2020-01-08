17c6de4ff9371a95bcac267b57c5a612a6324571
petrikovandrey <55155796+petrikovandrey@users.noreply.github.com>
2020-01-08 17:39:24 +0200

    заготовка

---
+++ 3.2-async/main.js
@@ -0,0 +1,41 @@
+class AlarmClock {
+    constructor() {
+        this.alarms = [],
+        this.id
+    }
+
+    addClock(timeStart, callBack, id) {
+        if (id === "undefind") {
+            console.error();
+            return;
+        }
+        //1 найти существующий звонок с id
+        //2 добавить в массив звонков новый
+    }
+
+    removeClock(id) {
+        //удалить звонок из массива
+    }
+
+    getCurrentFormattedTime() {
+        //today.getTime()
+        return
+    }
+
+    start() {
+
+    }
+
+    stop() {
+
+    }
+
+    printAlarms() {
+
+    }
+    
+    clearAlarms() {
+
+    }
+
+}
