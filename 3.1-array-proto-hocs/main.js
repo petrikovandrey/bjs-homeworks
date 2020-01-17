b40b8e90cb36c0fead3b5fb8f31dc84043a1faad
Andrey Petrikov <aapetrikov@gmail.com>
2020-01-17 22:26:05 +0200

    before test

--- 3.1-array-proto-hocs/main.js
+++ 3.1-array-proto-hocs/main.js
@@ -5,7 +5,7 @@ function sleep(milliseconds) {
 
 function sum(...args) {
     // Замедление на половину секунды.
-    sleep(500); // Можно использовать другое значение замедления.
+    // sleep(500); // Можно использовать другое значение замедления.
     return args.reduce((sum, arg) => {
         return sum += +arg;
     }, 0);
@@ -24,45 +24,54 @@ console.log(compareArrays([8, 1, 2], [8, 1, 2]));
 
 function memorize(fn, limit) {
 
-    let results = [
-        {
-            args: [],
-            result: undefined
-        }
-    ];
+    const results = [];
 
-    return function optimSum(...args) {
-        // console.log(results[0].args);
-        let result = results.find(compareArrays(elements[index],args));
-            // function compareArr(elements) {
-            //     console.log(args);
-            //     return elements.args.every((value, index) => value === args[index]);
-            // });
+    return function (...args) {
+        const result = results.find((value, index) => {
+            return value.args[index] === args[index];
+        });
 
-        // console.log(result);
-
-        if (result === "undefined") {         
-        //    console.log("1");
-            const sum = fn(args);
-            results.push(
-                {
-                    args: args,
-                    result: sum
-                });
-
-        } else {
-            // console.log("2");     
+        console.log(result);
+        if (result) {
+            console.log("I finded result");
             return result.result;
-        }
-
-        if (results.length === limit) {
-            results = result.slice(1);
         };
-        return sum;
+        console.log("I will be make calculations");
+        //const arg = args.join(', ');
+        //console.log("in memorize " + arg);
+        const calc = fn(arg);
+        console.log("sum " + calc); 
+
+        results.push(
+            {
+                args: args,
+                result: calc
+            });
+
+        if (results.length - 1 > limit) {
+            console.log("the memory is fully");
+            results = result.slice(results.length - limit);
+        };
+        return calc;
     }
 }
 
-const limit = 5;
-const mSum = memorize(sum, limit);
-// console.log(mSum([[2, 5, 3],[2, 5, 3],[2, 5, 3]]));
-console.log(mSum(10, 10, 10));
+
+function testCase() {
+    const arrayForTest =[
+        [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] 
+    ];
+    const mSum = memorize(sum, 3);
+
+    console.time("testCase");
+    let i = 10;
+    while (i) {
+        i--;
+        arrayForTest.forEach(item => mSum(item));
+    }
+
+    console.timeEnd("testCase");
+
+}
+
+testCase();
